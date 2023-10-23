111cb64ae8635926362e8f304ed0320e4bc3631e
zayman2045 <xavierorlandogriffith@gmail.com>
2023-10-23 00:10:17 -0400

    Spawn player ship in the center of the window.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
+        .run()
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/playerShip1_blue.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
