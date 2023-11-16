dac828ddb674d5242ab151c71a9ba428562ae8c3
zayman2045 <xavierorlandogriffith@gmail.com>
2023-11-16 17:53:14 -0500

    Add title and images to main menu.

--- src/main_menu/systems/layout.rs
+++ src/main_menu/systems/layout.rs
@@ -1,12 +1,9 @@
 use bevy::prelude::*;
 
-use crate::main_menu::{
-    components::*,
-    styles::{BUTTON_STYLE, NORMAL_BUTTON_COLOR},
-};
+use crate::main_menu::{components::*, styles::*};
 
 pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let main_menu_entity = build_main_menu(&mut commands, &asset_server);
+    let _main_menu_entity = build_main_menu(&mut commands, &asset_server);
 }
 
 pub fn despawn_main_menu(mut commands: Commands, main_menu_query: Query<Entity, With<MainMenu>>) {
@@ -19,23 +16,44 @@ pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>)
     let main_menu_entity = commands
         .spawn((
             NodeBundle {
-                style: Style {
-                    flex_direction: FlexDirection::Column,
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                    gap: Size::new(Val::Px(8.0), Val::Px(8.0)),
-                    ..default()
-                },
-                background_color: Color::BLACK.into(),
-
+                style: MAIN_MENU_STYLE,
                 ..default()
             },
             MainMenu {},
         ))
         .with_children(|parent| {
             // ==== Title ===
-
+            parent
+                .spawn(NodeBundle {
+                    style: TITLE_STYLE,
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Image 1
+                    parent.spawn(ImageBundle {
+                        style: IMAGE_STYLE,
+                        image: asset_server.load("sprites/playerShip1_blue.png").into(),
+                        ..default()
+                    });
+                    // Text
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "STARGAZE",
+                                get_title_text_style(&asset_server),
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                    // Image 2
+                    parent.spawn(ImageBundle {
+                        style: IMAGE_STYLE,
+                        image: asset_server.load("sprites/playerShip1_blue.png").into(),
+                        ..default()
+                    });
+                });
             // ==== Play Button ===
             parent
                 .spawn((
@@ -51,11 +69,7 @@ pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>)
                         text: Text {
                             sections: vec![TextSection::new(
                                 "Play",
-                                TextStyle {
-                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                    font_size: 32.0,
-                                    color: Color::WHITE,
-                                },
+                                get_button_text_style(&asset_server),
                             )],
                             alignment: TextAlignment::Center,
                             ..default()
@@ -78,11 +92,7 @@ pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>)
                         text: Text {
                             sections: vec![TextSection::new(
                                 "Quit",
-                                TextStyle {
-                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                                    font_size: 32.0,
-                                    color: Color::WHITE,
-                                },
+                                get_button_text_style(&asset_server),
                             )],
                             alignment: TextAlignment::Center,
                             ..default()
