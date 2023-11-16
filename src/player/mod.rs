8375fd930c7d09b5aa602644d7deeb97653ed20e
zayman2045 <xavierorlandogriffith@gmail.com>
2023-11-15 21:32:04 -0500

    Order player movement systems to prevent glitching.

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -11,7 +11,7 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(spawn_player)
             .add_system(player_movement)
-            .add_system(confine_player_movement)
+            .add_system(confine_player_movement.after(player_movement))
             .add_system(asteroid_hit_player)
             .add_system(player_collect_star);
 
