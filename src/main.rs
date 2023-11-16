69d8b10a4b4b45e3ef7251d4bc6719bfe1464444
zayman2045 <xavierorlandogriffith@gmail.com>
2023-11-16 09:19:05 -0500

    Define AppState state.

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,24 @@ use systems::*;
 
 fn main() {
     App::new()
+        // Bevy Plugins
         .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
+        // My Plugins
         .add_plugin(GamePlugin)
         .add_plugin(MainMenuPlugin)
+        // Startup Systems
         .add_startup_system(spawn_camera)
+        // Systems
         .add_system(exit_game)
         .add_system(handle_game_over)
         .run()
 }
 
+#[derive(States, Debug, Clone, Copy, Hash, PartialEq, Eq, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
+}
