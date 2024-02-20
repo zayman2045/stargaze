a2dffb23fcd81e1bf1cbd26ae273c92cf0180030
zayman2045 <xavierorlandogriffith@gmail.com>
2024-02-20 10:15:48 -0500

    Define exact spawn locations for initial asteroid.

--- src/game/asteroids/systems.rs
+++ src/game/asteroids/systems.rs
@@ -17,15 +17,16 @@ pub fn spawn_asteroids(
     asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
+    let (window_width, window_height) = (window.width(), window.height());
+    let mut initial_spawn_locations = [(0.1, 0.1), (0.1, 0.9), (0.9, 0.1), (0.9, 0.9)].iter().map(|(x, y)| (x * window_width, y * window_height));
 
     for _ in 0..NUMBER_OF_ASTEROIDS {
-        let random_x = random::<f32>() * window.width();
-        let random_y = random::<f32>() * window.height();
+        let (x, y) = initial_spawn_locations.next().expect("There should be one unique spawn location per asteroid");
 
         // Create a new entity with the SpriteBundle and Asteroid components
         commands.spawn((
             SpriteBundle {
-                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                transform: Transform::from_xyz(x, y, 0.0),
                 texture: asset_server.load("sprites/meteorBrown_big1.png"),
                 ..default()
             },
