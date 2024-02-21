1aa2189819b39f725d32ff2f85bd3de462862580
zayman2045 <xavierorlandogriffith@gmail.com>
2024-02-21 14:27:50 -0500

    Test insert_score and remove_score.

--- src/game/score/systems.rs
+++ src/game/score/systems.rs
@@ -12,3 +12,48 @@ pub fn insert_score(mut commands: Commands) {
 pub fn remove_score(mut commands: Commands) {
     commands.remove_resource::<Score>();
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert_score_adds_resource_to_world() {
+        // Setup app
+        let mut app = App::new();
+
+        // Add systems
+        app.add_systems(Update, insert_score);
+
+        // Verify resource was not previously present
+        assert!(!app.world.contains_resource::<Score>());
+
+        // Run startup systems to insert the Score resource
+        app.update();
+
+        // Check resulting changes
+        assert!(app.world.contains_resource::<Score>());
+    }
+
+    #[test]
+    fn remove_score_removes_resource_from_world() {
+        // Setup app
+        let mut app = App::new();
+
+        // Insert Score resource
+        app.insert_resource(Score::default());
+
+        // Verify resource is present
+        assert!(app.world.contains_resource::<Score>());
+
+        // Add systems
+        app.add_systems(Update, remove_score);
+
+        // Run startup systems to remove the Score resource
+        app.update();
+
+        // Check resulting changes
+        assert!(!app.world.contains_resource::<Score>());
+    }
+}
